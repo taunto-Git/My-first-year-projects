0ef7b5c2c60429725b087ea8191a5999dc374e79
Taunto <arturouro18@gmail.com>
2025-09-20 04:18:17 +0200

    librari created by me
    library to do a cout,cin of a determined type of variable in just one line

---
+++ Check_value.h
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+
+//library for doing a cout,cin, and also checking if the type of value is right
+
+int check_int(string frase) {
+	int test;
+	cout << frase << endl;
+	while (true)
+	{
+		cin >> test;
+		if (cin.fail()) {
+        	(cin.clear());
+        	cin.ignore(1000,'\n');
+        	cout << "not a valid integer" << endl;
+		}
+		else {
+			break;
+		}
+	}
+	return test;
+}
+
+float check_float(string frase) {
+	float test;
+	cout << "frase" << endl;
+	while (true)
+	{
+		cin >> test;
+		if (cin.fail()) {
+        	(cin.clear());
+        	cin.ignore(1000,'\n');
+        	cout << "not a valid float" << endl;
+		}
+		else {
+			break;
+		}
+	}
+
+	return test;
+}
+
+double check_double(string frase) {
+	double test;
+	cout << frase << endl;
+	while (true)
+	{
+		cin >> test;
+		if (cin.fail()) {
+        	(cin.clear());
+        	cin.ignore(1000,'\n');
+        	cout << "valore impossibile" << endl;
+		}
+		else {
+			break;
+		}
+	}
+	return test;
+}
+
+char check_char(string frase) {
+	char test;
+	cout << frase << endl;
+	while (true)
+	{
+		cin >> test;
+		if (cin.fail()) {
+			(cin.clear());
+			cin.ignore(1000,'\n');
+			cout << "not a valid char" << endl;
+		}
+		else {
+			break;
+		}
+	}
+	return test;
+	
+}
